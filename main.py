c94cf272f3b23b32f72fdc6bce847b5b0ee127ac
pereztomer <pereztomer3@gmail.com>
2022-11-11 19:02:55 +0200

    adding ipynb files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
 
+
 def main():
     df = pd.read_csv('data/Cities.csv')
 
@@ -8,5 +9,23 @@ def main():
     profile
 
 
+def merge_csvs():
+    Bids = pd.read_csv('data/Bids.csv')
+    Devices = pd.read_csv('data/Devices.csv')
+    Imps = pd.read_csv('data/Imps.csv')
+
+    Bids_Devices = pd.merge(Bids,
+                            Devices,
+                            on='DeviceID',
+                            how='outer')
+
+    Bids_Devices_Imps = pd.merge(Bids_Devices,
+                                 Imps,
+                                 on='ImpID',
+                                 how='outer')
+
+    Bids_Devices_Imps.to_csv('data/Bids_Devices_Imps.csv')
+
+
 if __name__ == '__main__':
-    main()
+    merge_csvs()
